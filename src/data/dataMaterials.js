1054ae77fa84be603f4acc42d073c4cfb9b749d0
robin-mk <robin.meltingk@gmail.com>
2023-09-26 12:17:30 +0200

    cleaning

--- src/data/dataMaterials.js
+++ src/data/dataMaterials.js
@@ -3,23 +3,6 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 const DataMaterials = () => {
   const materials = {
-    gravel: {
-      aoMap: useLoader(TextureLoader, 'materials/gravel/ambientOcclusion.jpg'),
-      roughnessMap: useLoader(TextureLoader, 'materials/gravel/roughness.jpg'),
-      normalMap: useLoader(TextureLoader, 'materials/gravel/normal.jpg'),
-      baseColorMap: useLoader(TextureLoader, 'materials/gravel/baseColor.jpg'),
-      diplacementMap: useLoader(TextureLoader, 'materials/gravel/height.png'),
-      metalnessMap: null,
-      metalness: 0,
-    },
-    fabric: {
-      aoMap: useLoader(TextureLoader, 'materials/fabric/ambientOcclusion.jpg'),
-      roughnessMap: useLoader(TextureLoader, 'materials/fabric/roughness.jpg'),
-      normalMap: useLoader(TextureLoader, 'materials/fabric/normal.jpg'),
-      baseColorMap: useLoader(TextureLoader, 'materials/fabric/baseColor.jpg'),
-      metalnessMap: null,
-      metalness: 0,
-    },
     wood: {
       aoMap: useLoader(TextureLoader, 'materials/wood/ambientOcclusion.jpg'),
       roughnessMap: useLoader(TextureLoader, 'materials/wood/roughness.jpg'),
